27d14e8a5573afe3b8853455ce08c774fb9c9f21
laith-hayajneh <farfesh96@gmail.com>
2021-09-04 23:21:31 +0300

    new

---
+++ src/routes/routes.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const { users } = require('../models');
+const basicAuth = require('../middleware/basic.js')
+const bearerAuth = require('../middleware/bearer.js')
+const permissions = require('../middleware/acl.js')
+
+authRouter.post('/signup', async (req, res, next) => {
+  console.log(req.body,'boooody')
+  try {
+    let userRecord = await req.body;
+    console.log(userRecord,'reeeee55555555555555555e');
+    const output = {
+      user: userRecord,
+      token: userRecord.token
+    };
+    res.status(201).json(output);
+    // res.send("ssssss")
+  } catch (e) {
+    console.log('object22222222222')
+    next(e.message)
+  }
+});
+
+authRouter.post('/signin', basicAuth, (req, res, next) => {
+  console.log('reeeee55555555555555555e');
+
+  const user = {
+    user: req.user,
+    token: req.user.token
+  };
+  res.status(200).json(user);
+});
+
+authRouter.get('/users', bearerAuth, permissions('delete'), async (req, res, next) => {
+  const userRecords = await users.findAll({});
+  const list = userRecords.map(user => user.username);
+  res.status(200).json(list);
+});
+
+authRouter.get('/secret',  async (req, res, next) => {
+  res.status(200).send('Welcome to the secret area')
+});
+
+module.exports = authRouter;
---
+++ src/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+
+// Esoteric Resources
+const notFoundHandler = require('./error-handlers/404.js');
+const errorHandler = require('./error-handlers/500.js');
+const logger = require('./middleware/logger.js');
+const authRoutes = require('./routes/routes.js');
+const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(express.json());
+
+app.use(logger);
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).send('This is the server');
+});
+
+app.use(authRoutes);
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
+
+// Catchalls
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: port => {
+    if (!port) { throw new Error('Missing Port'); }
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  },
+};
